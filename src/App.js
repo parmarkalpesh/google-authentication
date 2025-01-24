8592d8d524d146f3b5572f6d43e6b7fa56ce1bd0
kalpeshparmar <parmarkalpesh1586@gmail.com>
2025-01-24 20:09:49 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { FcGoogle } from "react-icons/fc";
+import { FiLogOut } from "react-icons/fi";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
+  console.log("Current User", user);
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isAuthenticated && <h2>Hello {user.name}</h2>}
+        {isAuthenticated ? (
+          <button
+            onClick={(e) => logout()}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px", // Space between icon and text
+              padding: "10px 15px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              cursor: "pointer",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              backgroundColor: "#fff",
+              color: "#000",
+            }}
+          >
+            <FiLogOut size={20} />
+            Logout
+          </button>
+        ) : (
+          <button
+            onClick={(e) => loginWithRedirect()}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              padding: "10px 15px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              cursor: "pointer",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              backgroundColor: "#fff",
+              color: "#000",
+            }}
+          >
+            <FcGoogle size={20} />
+            login With Google
+          </button>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
