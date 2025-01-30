5a750e7fecf9ed00a1e240f1567bb30028400e9f
kalpeshparmar <parmarkalpesh1586@gmail.com>
2025-01-30 22:44:40 +0530

    EditLink

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {isAuthenticated && <h2>Hello {user.name}</h2>}
+        {isAuthenticated && <h2>Hello {user.name}</h2> }
         {isAuthenticated ? (
           <button
             onClick={(e) => logout()}
